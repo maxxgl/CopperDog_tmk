ec461ded9ba953ba436f382554f2aa9eda372f96
Maxx Lehmann <mglehman@mtu.edu>
2017-01-16 00:20:06 -0600

    version 1

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -1,23 +1,24 @@
 #include "keymap_common.h"
+#include "bootloader.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
-    KEYMAP_ANSI(
-        ESC,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSlS, GRV,VOLU, \
+    KEYMAP(
+        ESC,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, GRV,VOLU, \
               TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSPC,VOLD, \
              CAPS,        A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN,QUOT, ENT,MUTE, \
              LSFT,        Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH,RSFT,  UP, FN0, \
              LCTL,LGUI,LALT,                SPC,          RALT,RGUI,RCTL,LEFT,DOWN,RGHT),
     /* 1: FN */
-    KEYMAP_ANSI(
+    KEYMAP(
         TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,TRNS,TRNS, \
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
              TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
              TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, FN2,TRNS,TRNS, FN0, \
              TRNS,TRNS,TRNS,                TRNS,         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS), 
     /* 2: ReProgram */
-    KEYMAP_ANSI(
-        TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,TRNS,TRNS, \
+    KEYMAP(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
              TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
              TRNS,      FN3,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
@@ -30,7 +31,7 @@ enum function_id {
 
 const action_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TOGGLE(1),        // to Fn overlay
-    [1] = ACTION_LAYER_MOMENTERY(2),     // toggle arrow overlay
+    [1] = ACTION_LAYER_MOMENTARY(2),     // toggle arrow overlay
     [2] = ACTION_FUNCTION(TEENSY_KEY),
 };
 
